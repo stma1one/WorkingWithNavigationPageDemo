ed30777ec57ff36f92e488dc9f9e3c90373d48f8
Home <Home@LAPTOP-2E6V6ACM>
2021-02-24 17:01:06 +0200

    Add project files.

---
+++ WorkingWithNavigationPageDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkingWithNavigationPageDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "Main Page";
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SubPage(), true);
+            
+            
+        }
+
+        
+    }
+}
---
+++ WorkingWithNavigationPageDemo/SubPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WorkingWithNavigationPageDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubPage : ContentPage
+    {
+        public SubPage()
+        {
+            InitializeComponent();
+            Title = "Sub Page";
+        }
+
+       
+
+        protected override void OnAppearing()
+        {
+            LblTxt.Text += $"\n entered Screen on {DateTime.Now.ToString()}";
+
+        }
+    }
+}
